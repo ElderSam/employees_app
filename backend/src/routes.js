a81eacb2ec26b3407582e0c3a321535286e7180a
ElderSam <eldersamuel98@gmail.com>
2020-09-26 10:14:25 -0300

    lendo o arquivo bd_funcionarios linha a linha

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -7,7 +7,34 @@ app.get("/", (req, res) => { res.send('Server is running!'); });
 
 app.get("/api", (req, res) => {
 
-    res.send('Hello API!');
+    var file = 'src/bd_funcionarios.txt';
+    readFile(file);
+
+    function readFile(file) {
+        
+        var arr = [];
+
+        const readline = require('readline');
+        const fs = require('fs');
+
+        const rl = readline.createInterface({
+            input: fs.createReadStream(file)
+        });
+
+        rl.on('line', function (line) { // para cada linha do arquivo
+
+            if(line) { //se a linha não está vazia
+                //console.log('Line from file:', line);
+                arr.push(line);
+            }
+        });
+
+        rl.on('close', function() { // quando termina a leitura do arquivo
+            
+            console.log('arr', arr);
+        });
+    }
+    
 });
 
 module.exports = routes;
