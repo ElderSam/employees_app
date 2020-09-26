38023fd2216b757e2ff859f96b0fa443cda753ef
ElderSam <eldersamuel98@gmail.com>
2020-09-25 22:51:57 -0300

    backend: mudando nome do arquivo inicial

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const express = require('express'); //adiciona o express
 app = express();
 
 app.get("/", (req, res) => {
-    res.send("Hello Nodemon!");
+    res.send('Server is running!');
 });
 
 app.listen(3001); //inicia o servidor na porta 3001
