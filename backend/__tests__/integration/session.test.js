fe2997893c4111269b96714892e740c38773f280
ElderSam <eldersamuel98@gmail.com>
2020-09-27 22:06:17 -0300

    criando primeiro teste com Jest

---
+++ backend/__tests__/integration/session.test.js
@@ -0,0 +1,25 @@
+require('dotenv').config({  
+    //path: process.env.NODE_ENV === "test" ? ".env.test" : ".env"
+    path: ".env.test"
+})
+
+const employee = require('./../../src//controllers/Employees');
+
+describe('Salvar funcionário', () => {
+    it('Insere um novo funcionário ou atualiza um existente', () => {
+
+        const aux = {
+            DataCad: "27/09/2020",
+            Cargo: "PO Jr",
+            Cpf: "347367548768",
+            Nome: "John",
+            UfNasc: "RS",
+            Salario: 5070.98,
+            Status: "EM ANÁLISE"
+        }
+
+        newEmployee = employee.saveEmployee(aux)
+
+        expect(newEmployee).toBe(aux)
+    });
+})
