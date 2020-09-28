e1832964465bc921c5d4c8043dab98cec659cea0
ElderSam <eldersamuel98@gmail.com>
2020-09-28 18:43:41 -0300

    testando inserir e atualizar um funcionário

--- backend/__tests__/integration/employees.test.js
+++ backend/__tests__/integration/employees.test.js
@@ -5,22 +5,41 @@ require('dotenv').config({
 
 const employee = require('./../../src//controllers/Employees');
 
-describe('Salvar funcionário', () => {
-    it('Insere um novo funcionário ou atualiza um existente, e deve retornar o próprio objeto', () => {
+/** ------------------------ TESTES DE INTEGRAÇÃO ---------------------------- 
+OBS: testa conjuntos de funcionalidades que podem ter algum efeito colateral */
 
-        const aux = {
-            DataCad: "27/09/2020",
-            Cargo: "PO Jr",
-            Cpf: "347367548768",
-            Nome: "John",
-            UfNasc: "RS",
-            Salario: 5070.98,
-            Status: "EM ANÁLISE"
-        }
+describe('Salvar funcionário', () => {
+
+    const aux = {
+        DataCad: "27/09/2020",
+        Cargo: "PO Jr",
+        Cpf: "347367548768",
+        Nome: "John",
+        UfNasc: "RS",
+        Salario: 5070.98,
+        Status: "EM ANÁLISE"
+    }
+    
+    it('Insere um novo funcionário', () => {
+        qtdInicial = employee.getEmployees().length
 
         newEmployee = employee.saveEmployee(aux)
 
         expect(newEmployee).toBe(aux)
+
+        qtdNova = employee.getEmployees().length
+        expect(qtdNova).toBe(qtdInicial + 1)
+    });
+
+    it('Atualiza um funcionário existente', () => {
+        qtdInicial = employee.getEmployees().length
+        aux.Cargo = "PO Pl";
+
+        res = employee.saveEmployee(aux)
+        expect(res).toBe(aux)
+
+        qtdNova = employee.getEmployees().length
+        expect(qtdNova).toBe(qtdInicial)
     });
 })
 
