210ba2999945bbc31d5f564829b5638080ebf716
ElderSam <eldersamuel98@gmail.com>
2020-09-29 18:04:11 -0300

    Criando estrutura dos Componentes (Template)

---
+++ frontend/src/components/template/Header.jsx
@@ -0,0 +1,8 @@
+import './Header.css';
+import React from 'react'
+
+export default props =>
+    <aside className="header">
+        Header
+    </aside>
+    
---
+++ frontend/src/components/template/Logo.jsx
@@ -0,0 +1,8 @@
+import './Logo.css';
+import React from 'react'
+
+export default props =>
+    <aside className="logo">
+        Logo
+    </aside>
+    
