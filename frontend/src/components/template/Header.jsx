dc94af1711b627396bd95978bacf479b1874bcdb
ElderSam <eldersamuel98@gmail.com>
2020-09-29 18:57:15 -0300

    componente Cabeçalho

--- frontend/src/components/template/Header.jsx
+++ frontend/src/components/template/Header.jsx
@@ -2,7 +2,10 @@ import './Header.css';
 import React from 'react'
 
 export default props =>
-    <aside className="header">
-        Header
-    </aside>
+    <header className="header d-none d-sm-flex flex-column">
+        <h1 className="mt-3">
+            <i className={`fa fa-${props.icon}`}></i> {props.title}
+        </h1>
+        <p className="lead text-muted">{props.subtitle}</p>
+    </header>
     
