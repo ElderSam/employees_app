bbdf5f8faf761596821686ebbd0bc3de5e74dfa7
ElderSam <eldersamuel98@gmail.com>
2020-09-29 19:33:25 -0300

    componente Navegação

--- frontend/src/main/App.jsx
+++ frontend/src/main/App.jsx
@@ -1,3 +1,5 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'font-awesome/css/font-awesome.min.css'
 import './App.css'
 import React from 'react'
 
@@ -10,6 +12,7 @@ export default props =>
     <div className="app">
         <Logo />
         <Nav />
-        <Main />
+        <Main icon="home" title="inicio"
+            subtitle="Projeto 'Funcionários App' em ReactJS"/>
         <Footer />
     </div>
