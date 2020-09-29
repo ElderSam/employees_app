b1821b51ee2b632c7a8a2d1e3102e3935ba7e0ac
ElderSam <eldersamuel98@gmail.com>
2020-09-29 19:03:34 -0300

    componente Rodapé

--- frontend/src/components/template/Footer.jsx
+++ frontend/src/components/template/Footer.jsx
@@ -3,5 +3,8 @@ import React from 'react'
 
 export default props =>
     <footer className="footer">
-        Footer
+        <span>
+            Desenvolvido com <i className="fa fa-heart text-danger"></i> por
+            <strong> Elder <span className="text-danger">Samuel</span></strong>
+        </span>
     </footer>
