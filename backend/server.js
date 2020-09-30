052f39468884edc16c7123e3703bdd6b410da083
ElderSam <eldersamuel98@gmail.com>
2020-09-30 00:18:52 -0300

    instalando CORS para permitir acesso à API externamente

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,12 @@ require('dotenv').config({
 })
 
 const express = require('express'); //adiciona o express
+const cors = require('cors'); //permite acesso de domínios externos
 
 app = express(); // inicia o app
+
+app.use(express.json()); //permite receber informações em JSON
+app.use(cors()); //cors() sem parâmetros permite acesso de todos os domínios, a API se torna pública
 
 app.use('/api', require('./src/routes'));
 
